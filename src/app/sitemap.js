21d6c6875ed847ab245328b6651e1ae836dcfff3
Abhishek Kumar <abhi.kumar252004@gmail.com>
2024-12-19 14:10:45 +0530

    all

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,26 +1,26 @@
 export default function sitemap() {
-    const domain = process.env.DOMAIN || 'https://localhost:30000'; // ensure DOMAIN is defined
+    const domain = process.env.DOMAIN ; // ensure DOMAIN is defined
   
     return [
       {
-        url: `${domain}/`,
+        url: 'https://sarkitshala.site/',
         changeFrequency: 'monthly',
         priority: 1,
       },
       {
-        url: `${domain}/arduino`,
+        url: 'https://sarkitshala.site/arduino',
         lastModified: new Date().toISOString(),  // using ISO string format
         changeFrequency: 'monthly',
         priority: 1,
       },
       {
-        url: `${domain}/raspberry`,
+        url: 'https://sarkitshala.site/raspberry',
         lastModified: new Date().toISOString(),  // using ISO string format
         changeFrequency: 'weekly',
         priority: 0.5,
       },
       {
-        url: `${domain}/esp`,
+        url: 'https://sarkitshala.site/esp',
         changeFrequency: 'weekly',
         priority: 1,
       },
