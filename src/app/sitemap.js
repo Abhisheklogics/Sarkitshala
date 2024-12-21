d58bfd30564c6edde58fc84eff93f08d648989ce
abhi.Logics <abhi.kumar252004@gmail.com>
2024-12-22 02:20:10 +0530

    all

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,24 +1,42 @@
 export default async function sitemap() {
  
+  let res= await fetch( `https://sarkitshala.site/api/experiments/Arduino`)
+  let data= await res.json()
+   let ardurl=data.map(({ExperimentId})=>({
+     url:`https://sarkitshala.site/arduino/${ExperimentId}`
+   }))
+    
+    
   
- 
-  return [
-    {
-      url: `https://sarkitshala.site/arduino`,
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-   
-    {
-      url: `https://sarkitshala.site/raspberry`,
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-    {
-      url: `https://sarkitshala.site/esp`,
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-   
-  ];
-}
+    
+  
+   let res2= await fetch( `https://sarkitshala.site/api/experiments/Esp`)
+  let data2= await res2.json()
+   let Espurl=data2.map(({ExperimentId})=>({
+     url:`https://sarkitshala.site/esp/${ExperimentId}`,
+     priority: 1,
+   }))
+    return [
+      {
+        url: `https://sarkitshala.site/arduino`,
+        changeFrequency: 'monthly',
+        priority: 1,
+      },
+      ...ardurl,
+      
+      {
+        url: `https://sarkitshala.site/raspberry`,
+        changeFrequency: 'monthly',
+        priority: 1,
+      },
+      
+      {
+        url: `https://sarkitshala.site/esp`,
+        changeFrequency: 'monthly',
+        priority: 1,
+      },
+      ...Espurl
+    
+    ];
+  }
+  
