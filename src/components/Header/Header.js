dac11379125393d32c2ac9930e28e391235b4b12
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-02-27 12:04:27 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,21 +20,21 @@ export default function Header() {
           <div className="flex md:order-1 space-x-3 md:space-x-0 rtl:space-x-reverse"></div>
 
           <div className="md:mt-[-12px] mt-[-5px] ml-4 items-center justify-between w-full text-gray-900 md:w-auto md:order-1" id="navbar-sticky">
-            <ul className="flex ml-[-15px] font-bold text-xl md:relative md:left-[500px] p-3 md:p-0  md:space-x-8 rtl:space-x-reverse md:mt-2 md:border-0 md:bg-none overflow-x-auto whitespace-nowrap overscroll-x-contain flex-nowrap">
+            <ul className="flex ml-[-15px] text-white font-bold text-xl md:relative md:left-[500px] p-3 md:p-0  md:space-x-8 rtl:space-x-reverse md:mt-2 md:border-0 md:bg-none overflow-x-auto whitespace-nowrap overscroll-x-contain flex-nowrap">
               <li className='hover:bg-orange-400 p-1 rounded'>
-                <Link href="/" className="block py-2 px-3  text-gray-900 rounded md:bg-transparent hover:text-white md:p-0">Home</Link>
+                <Link href="/" className="block py-2 px-3  rounded md:bg-transparent hover:text-white md:p-0">Home</Link>
               </li>
               <li className='hover:bg-orange-400 p-1 rounded'>
-                <Link href="/arduino" className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-white md:p-0">Arduino</Link>
+                <Link href="/arduino" className="block py-2 px-3  rounded md:hover:bg-transparent md:hover:text-white md:p-0">Arduino</Link>
               </li>
               <li className='hover:bg-orange-400 p-1 rounded'>
-                <Link href="/raspberry" className="block py-2 px-2 text-gray-900 rounded md:hover:bg-transparent md:hover:text-white md:p-0">Raspberry Pi</Link>
+                <Link href="/raspberry" className="block py-2 px-2  rounded md:hover:bg-transparent md:hover:text-white md:p-0">Raspberry Pi</Link>
               </li>
               <li className='hover:bg-orange-400 p-1 rounded'>
-                <Link href="/esp" className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-white md:p-0">Esp32</Link>
+                <Link href="/esp" className="block py-2 px-3  rounded md:hover:bg-transparent md:hover:text-white md:p-0">Esp32</Link>
               </li>
               <li className='hover:bg-orange-400 p-1 rounded '>
-                <Link href="https://codeeditor-five.vercel.app/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Compiler</Link>
+                <Link href="https://codeeditor-five.vercel.app/" className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Compiler</Link>
               </li>
             </ul>
           </div>
