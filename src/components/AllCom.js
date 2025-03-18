3cb03d304ccd51f897e48f1e9511e592ecb96dd4
Abhishek Kumar <abhi.kumar252004@gmail.com>
2025-03-18 15:47:37 +0530

    all

--- src/components/AllCom.js
+++ src/components/AllCom.js
@@ -23,7 +23,7 @@ export default function AllCom({data})
       </section>
       <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
         <h2 className="text-xl font-bold mb-6 ">{data.title4}</h2>
-        {Object.values(data.CC).map((val)=>(
+        {data.CC &&  Object.values(data.CC).map((val)=>(
           <p key={data.ExperimentId}>{val.name}</p>
         ))}
       
@@ -31,7 +31,7 @@ export default function AllCom({data})
       <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
         <h2 className="text-xl font-bold mb-6 ">Common Anode RGB LED
         </h2>
-        {Object.values(data.CA).map((val)=>(
+        {data.CA &&  Object.values(data.CA).map((val)=>(
           <p key={data.ExperimentId}>{val.name}</p>
         ))}
       
@@ -39,11 +39,74 @@ export default function AllCom({data})
       <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
         <h2 className="text-xl font-bold mb-6 ">{data.title5}
         </h2>
-        {Object.values(data.CA).map((val)=>(
-          <p key={data.ExperimentId}>{val.name}</p>
-        ))}
+        
+          <p>{data.para3}</p>
+     
       
       </section>
+      <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
+        <h2 className="text-xl font-bold mb-6 ">{data.title6}
+        </h2>
+        
+          {data.IDC && Object.values(data.IDC).map((val)=>(
+            <p key={data.ExperimentId}>{val.name}</p>
+          ))}
+     
+      
+      </section>
+      <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
+        <h2 className="text-xl font-bold mb-6 ">{data.title7}
+        </h2>
+            <p>{data.para4}</p>  
+            { data.WA && Object.values(data.WA).map((val)=>(
+            <p key={data.ExperimentId}>{val.name}</p>
+          ))}
+     
+      </section>
+      <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
+        
+      <h2 className="text-xl font-bold mb-6 ">{data.title8}
+         
+      </h2>
+      </section>
+      <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
+        
+        <h2 className="text-xl font-bold mb-6 ">{data.title9}
+           
+        </h2>
+        </section>
+        <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
+        
+        <h2 className="text-xl font-bold mb-2 ">{data.title10}      
+        </h2>
+        { data.CM && Object.values(data.CM).map((val)=>(
+            <p key={data.ExperimentId}>{val.name}</p>
+          ))}
+        </section>
+        <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
+        
+        <h2 className="text-xl font-bold mb-2 ">{data.title11}      
+        </h2>
+        {data.URP &&  Object.values(data.URP).map((val)=>(
+            <p key={data.ExperimentId}>{val.name}</p>
+          ))}
+        </section>
+        <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
+        
+        <h2 className="text-xl font-bold mb-2 ">{data.title12}      
+        </h2>
+        { data.CCA && Object.values(data.CCA).map((val)=>(
+            <p key={data.ExperimentId}>{val.name}</p>
+          ))}
+        </section>
+        <section className="mb-6 mt-10 max-w-4xl bg-white p-6 rounded shadow-lg border border-black">
+        
+        <h2 className="text-xl font-bold mb-2 ">{data.title13}      
+        </h2>
+        { data.CCAA &&  Object.values(data.CCAA).map((val)=>(
+            <p key={data.ExperimentId}>{val.name}</p>
+          ))}
+        </section>
         </div>
         <Side/>
         </>
