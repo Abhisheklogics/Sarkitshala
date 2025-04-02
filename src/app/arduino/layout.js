603fb90f5eac6e860e623277a6b9ea95ab80be7d
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-04-02 12:10:44 +0530

    layout.js

--- src/app/arduino/layout.js
+++ src/app/arduino/layout.js
@@ -85,6 +85,7 @@ export default function RootLayout({ children }) {
         { href: `/arduino/${'interfacing-metal-touch-sensor'}`, text: "Interfacing Metal Touch Sensor Module" },
         { href: `/arduino/${'interfacing-line-sensor-module'}`, text: "Interfacing Line Sensor Module" },
         { href: `/arduino/${'interfacing-linear-magnetic-hall-sensor'}`, text: "Interfacing Liner Magnetic Hall Sensor Module" },
+        { href: `/arduino/${'interfacing-thermostat-switch'}`, text: "Interfacing Digital Microcomputer Thermostat switch" },
 
         
       
