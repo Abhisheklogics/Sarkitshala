a03248c767a3c79449a18b3acc43492217dd0203
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-04-02 13:29:15 +0530

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
         <meta name="google-site-verification" content="Mt7iRB_iff5dtyKWL3AH1Isf0HycfwBBgnRMpobxba4" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
-      <body className={`${inter.className} md:bg-[#FFF0E5]`}>
+      <body className={`${inter.className} font-serif md:bg-[#FFF0E5]`}>
         <Header />
         <main>{children}</main>
       </body>
