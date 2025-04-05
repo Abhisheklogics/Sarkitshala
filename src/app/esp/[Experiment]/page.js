d3989fff7bc84cd2441cd54174ac808da0a6cced
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-04-05 16:19:28 +0530

    page.js

--- src/app/esp/[Experiment]/page.js
+++ src/app/esp/[Experiment]/page.js
@@ -111,11 +111,9 @@ export default async function Page({ params }) {
   
   
   const data = await getData(`https://sarkitshala.com/api/experiments/Esp`, Experiment)
-   if(data['ExperimentId' == '10'])
+   if(data['ExperimentId'] == '10'])
    {
-     return(
-       <esp data={data}/>
-     )
+     return(<esp data={data}/>)
    }
 
   return (
