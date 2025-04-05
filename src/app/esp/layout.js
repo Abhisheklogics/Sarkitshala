9e32f28a8866660851276066fe6fef14c61a493e
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-04-05 15:40:18 +0530

    layout.js

--- src/app/esp/layout.js
+++ src/app/esp/layout.js
@@ -57,6 +57,7 @@ export default function RootLayout({ children }) {
         { href: "/esp/servo-motor", text: "Interfacing Servo Motor" },
          { href: "/esp/esp32-cam-wireless", text: "Interfacing Cam Wireless" },
         { href: "/esp/esp32-dc-motor", text: "Interfacing DC Motor" },
+        { href: "interfacing-shock-sensor", text: "Interfacing Shock Sensor" },
       ],
     },
    
