4f401dfe213e2540c408ad599f612ce8c5d42640
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-04-18 19:13:25 +0530

    Esp.js

--- src/components/Esp.js
+++ src/components/Esp.js
@@ -1,21 +1,20 @@
 import dynamic from "next/dynamic";
-import Image from "next/image"
+import   '../app/globals.css'
 const CodeBox = dynamic(() => import('@/components/code/code'), {
   ssr: false
 });
 
 export default function Esp({ data }) {
   return (
-    <div className="h-fit w-full p-5 bg-cover rounded-xl leading-8 text-justify break-words 
-        md:w-[790px] md:ml-[330px] md:mt-24 2xl:ml-[430px] md:bg-fixed">
+    <div className="w-full p-4 md:p-5 bg-white md:bg-[#FFF0E5] rounded-xl leading-8 text-justify break-words font-serif h-fit overflow-y-auto 
+                md:w-[790px] md:ml-[330px] md:mt-20 
+                2xl:w-[820px] 2xl:ml-[430px]">
       
       <h1 className="text-3xl font-bold mb-4 text-center">{data.mainTitle}</h1>
-  {data.image1 && <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
-   <Image height={400} width={700} className="md:ml-2 md:mt-2"  src={data.image1} alt={`${data.ExperimentName}`} />
- </section>}
+
       {/* Title 1 - Introduction */}
       {data.title1 && (
-        <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
+        <section className="containerClass">
           <h2 className="text-xl font-bold mb-4">{data.title1}</h2>
           <p>{data.para1}</p>
         </section>
@@ -23,7 +22,7 @@ export default function Esp({ data }) {
 
       {/* Title 2 - Working Principle */}
       {data.title2 && (
-        <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
+        <section className="containerClass">
           <h2 className="text-xl font-bold mb-4">{data.title2}</h2>
           <p>{data.para2}</p>
 
@@ -42,13 +41,10 @@ export default function Esp({ data }) {
           )}
         </section>
       )}
- {data.image2 && <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
-        <Image height={400} width={700} className="md:ml-4 md:mt-2"  src={data.image2} alt={`${data.ExperimentName}`} /> 
-         </section>}
 
       {/* Title 3 - Components Required */}
       {data.title3 && data.components?.length > 0 && (
-        <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
+        <section className="containerClass">
           <h2 className="text-xl font-bold mb-4">{data.title3}</h2>
           <ul className="list-disc pl-6">
             {data.components.map((item, idx) => (
@@ -59,7 +55,7 @@ export default function Esp({ data }) {
       )}
 
       {data.title4 && data.pinConfig?.length > 0 && (
-        <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
+        <section className="containerClass">
           <h2 className="text-xl font-bold mb-4">{data.title4}</h2>
           <ul className="list-disc pl-6">
             {data.pinConfig.map((item, idx) => (
@@ -71,12 +67,10 @@ export default function Esp({ data }) {
           {data.note && <p className="mt-4 italic text-sm text-gray-700">{data.note}</p>}
         </section>
       )}
- {data.image3 && <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
-   <Image height={600} width={400} className="md:mt-20 md:ml-40 mt-8 rounded"  src={data.image3} alt={`${data.ExperimentName} - Pin Diagram`}/>
-     </section>}
+
       {/* Title 5 - Wiring */}
       {data.title5 && data.wiring?.length > 0 && (
-        <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
+        <section className="containerClass">
           <h2 className="text-xl font-bold mb-4">{data.title5}</h2>
           <ul className="list-decimal pl-6">
             {data.wiring.map((item, idx) => (
@@ -87,12 +81,10 @@ export default function Esp({ data }) {
           </ul>
         </section>
       )}
-{data.image4 && <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
- <Image height={400} width={500} className="md:ml-24    mt-10"  src={data.image4} alt={`${data.ExperimentName} - Circuit Diagram`} />
- </section>}
+
       {/* Title 6 - Arduino Code */}
       {data.title6 && data.code && (
-        <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
+        <section className="containerClass">
           <h2 className="text-xl font-bold mb-4">{data.title6}</h2>
           {data.code && 
             <CodeBox code={data.code.replace(/\\n/g, '\n')} language={'cpp'} />
@@ -102,7 +94,7 @@ export default function Esp({ data }) {
 
       {/* Title 7 - Code Explanation */}
       {data.title7 && data.codeExplanation?.length > 0 && (
-        <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
+        <section className="containerClass">
           <h2 className="text-xl font-bold mb-4">{data.title7}</h2>
           <ul className="list-decimal pl-6">
             {data.codeExplanation.map((item, idx) => (
@@ -116,7 +108,7 @@ export default function Esp({ data }) {
 
       {/* Title 8 - Applications */}
       {data.title8 && data.applications?.length > 0 && (
-        <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
+        <section className="containerClass">
           <h2 className="text-xl font-bold mb-4">{data.title8}</h2>
           <ul className="list-disc pl-6">
             {data.applications.map((item, idx) => (
@@ -128,7 +120,7 @@ export default function Esp({ data }) {
 
       {/* Title 9 - Conclusion */}
       {data.title9 && data.conclusion && (
-        <section className="mb-6 bg-white p-6 rounded shadow-lg border border-black">
+        <section className="containerClass">
           <h2 className="text-xl font-bold mb-4">{data.title9}</h2>
           <p>{data.conclusion}</p>
         </section>
