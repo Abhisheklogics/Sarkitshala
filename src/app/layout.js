cd6b7a157a65e12e00260986ac0eedad1a71700d
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-05-17 02:28:16 +0530

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,12 +9,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-           <meta name="google-adsense-account" content="ca-pub-2053631970024775"/>
-        <meta name="google-site-verification" content="Mt7iRB_iff5dtyKWL3AH1Isf0HycfwBBgnRMpobxba4" />
-   
+      
+        <meta
+          name="google-site-verification"
+          content="Mt7iRB_iff5dtyKWL3AH1Isf0HycfwBBgnRMpobxba4"
+        />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className={`${inter.className} font-serif md:bg-[#FFF0E5]`}>
+      
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2053631970024775"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         <Header />
         <main>{children}</main>
       </body>
