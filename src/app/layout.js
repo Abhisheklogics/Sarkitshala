26b71f0c2d3b686999fc730b4811e617279443a4
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-05-17 02:58:12 +0530

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,21 +9,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      
-        <meta
-          name="google-site-verification"
-          content="Mt7iRB_iff5dtyKWL3AH1Isf0HycfwBBgnRMpobxba4"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </head>
-      <body className={`${inter.className} font-serif md:bg-[#FFF0E5]`}>
-      
+     
         <Script
+          strategy="afterInteractive"
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2053631970024775"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
+
+        <meta name="google-site-verification" content="Mt7iRB_iff5dtyKWL3AH1Isf0HycfwBBgnRMpobxba4" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
+
+      <body className={`${inter.className} font-serif md:bg-[#FFF0E5]`}>
         <Header />
         <main>{children}</main>
       </body>
