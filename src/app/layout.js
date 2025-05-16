d68d9136c317af050469fcb8bc6ac5de969c1ea8
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-05-17 02:24:20 +0530

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,9 +8,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        
+           <meta name="google-adsense-account" content="ca-pub-2053631970024775"/>
         <meta name="google-site-verification" content="Mt7iRB_iff5dtyKWL3AH1Isf0HycfwBBgnRMpobxba4" />
-      <meta name="google-adsense-account" content="ca-pub-2053631970024775"/>
+   
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className={`${inter.className} font-serif md:bg-[#FFF0E5]`}>
